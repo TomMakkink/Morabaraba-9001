dfe337ce59ff931055998a98f45d0834dd885e77
TomMakkink <tommakkink@hotmail.com>
2018-04-12 16:37:46 +0200

    Created Umpire class and interface

---
+++ Morabaraba3/moraba/IUmpire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace moraba
+{
+    interface IUmpire
+    {
+        IPlayer Win();
+        bool isDraw();
+        void play();
+    }
+}
---
+++ Morabaraba3/moraba/Umpire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace moraba
+{
+    class Umpire : IUmpire
+    {
+        public bool isDraw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayer Win()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
