e821e12ea8ff2588c176a9e3fab88cfde0b2f1e9
desluk <lukedestombe.ldt@gmail.com>
2018-04-14 15:28:46 +0200

    Altered Umpire

--- Morabaraba3/moraba/Umpire.cs
+++ Morabaraba3/moraba/Umpire.cs
@@ -9,14 +9,70 @@ namespace moraba
         public int turns;
         public Player currentPlayer;
         public Player enemy;
+        Board board;
+        private List<List<string>> getMillOptions(int index)
+        {
+            string caseSwitch = board.getMainNodeList()[index].Position;
 
-        public Umpire()
+            switch (caseSwitch)
+            {
+                case "a0":
+                    return new List<List<string>> { new List<string> { "a0", "a3", "a6" }, new List<string> { "a0", "d0", "g0" }, new List<string> { "a0", "b1", "c2" } };
+                case "a3":
+                    return new List<List<string>> { new List<string> { "a0", "a3", "a6" }, new List<string> { "a3", "b3", "c3" } };
+                case "a6":
+                    return new List<List<string>> { new List<string> { "a0", "a3", "a6" }, new List<string> { "a6", "g6", "d6" }, new List<string> { "a6", "b5", "c4" } };
+                case "b1":
+                    return new List<List<string>> { new List<string> { "b1", "b3", "b5" }, new List<string> { "a0", "b1", "c2" }, new List<string> { "b1", "d1", "f1" } };
+                case "b3":
+                    return new List<List<string>> { new List<string> { "b1", "b3", "b5" }, new List<string> { "a3", "b3", "c3" } };
+                case "b5":
+                    return new List<List<string>> { new List<string> { "b1", "b3", "b5" }, new List<string> { "b5", "a6", "c4" }, new List<string> { "b5", "f5", "d5" } };
+                case "c2":
+                    return new List<List<string>> { new List<string> { "c2", "c3", "c4" }, new List<string> { "c2", "d2", "e2" }, new List<string> { "a0", "b1", "c2" } };
+                case "c3":
+                    return new List<List<string>> { new List<string> { "c2", "c3", "c4" }, new List<string> { "a3", "b3", "c3" } };
+                case "c4":
+                    return new List<List<string>> { new List<string> { "c2", "c3", "c4" }, new List<string> { "c4", "d4", "e4" }, new List<string> { "c4", "b5", "a6" } };
+                case "d0":
+                    return new List<List<string>> { new List<string> { "d0", "d1", "d2" }, new List<string> { "a0", "d0", "g0" } };
+                case "d1":
+                    return new List<List<string>> { new List<string> { "d0", "d1", "d2" }, new List<string> { "b1", "d1", "f1" } };
+                case "d2":
+                    return new List<List<string>> { new List<string> { "d0", "d1", "d2" }, new List<string> { "d2", "e2", "c2" } };
+                case "d4":
+                    return new List<List<string>> { new List<string> { "d4", "d5", "d6" }, new List<string> { "d4", "c4", "e4" } };
+                case "d5":
+                    return new List<List<string>> { new List<string> { "d4", "d5", "d6" }, new List<string> { "d5", "b5", "f5" } };
+                case "d6":
+                    return new List<List<string>> { new List<string> { "d4", "d5", "d6" }, new List<string> { "a6", "d6", "g6" } };
+                case "e2":
+                    return new List<List<string>> { new List<string> { "e2", "e3", "e4" }, new List<string> { "e2", "f1", "g0" }, new List<string> { "e2", "d2", "c2" } };
+                case "e3":
+                    return new List<List<string>> { new List<string> { "e2", "e3", "e4" }, new List<string> { "e3", "f3", "g3" } };
+                case "e4":
+                    return new List<List<string>> { new List<string> { "e2", "e3", "e4" }, new List<string> { "e4", "f5", "g6" }, new List<string> { "e4", "d4", "c4" } };
+                case "f1":
+                    return new List<List<string>> { new List<string> { "f1", "f3", "f5" }, new List<string> { "f1", "e2", "g0" }, new List<string> { "f1", "d1", "b1" } };
+                case "f3":
+                    return new List<List<string>> { new List<string> { "f1", "f3", "f5" }, new List<string> { "g3", "e3", "f3" } };
+                case "f5":
+                    return new List<List<string>> { new List<string> { "f1", "f3", "f5" }, new List<string> { "f5", "e4", "g6" }, new List<string> { "f5", "d5", "b5" } };
+                case "g0":
+                    return new List<List<string>> { new List<string> { "g0", "g3", "g6" }, new List<string> { "g0", "f1", "e2" }, new List<string> { "g0", "a0", "d0" } };
+                case "g3":
+                    return new List<List<string>> { new List<string> { "g0", "g3", "g6" }, new List<string> { "g3", "f3", "e3" } };
+                case "g6":
+                    return new List<List<string>> { new List<string> { "g0", "g3", "g6" }, new List<string> { "g6", "d6", "a6" }, new List<string> { "g6", "f5", "e4" } };
+
+
+            }
+            return new List<List<string>> { };
+        }
+        public Umpire(Board b)
         {
             turns = 1;
-        }
-        public bool isDraw()
-        {
-            throw new NotImplementedException();
+            board = b;
         }
 
         public void play(Player player1, Player player2)
@@ -32,6 +88,8 @@ namespace moraba
                 enemy = player1;
             }
         }
+
+        
 
         public Player Win()
         {
