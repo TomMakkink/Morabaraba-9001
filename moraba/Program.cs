e84efa97214e39b802413f39fd5fac4b3849f292
TomMakkink <tommakkink@hotmail.com>
2018-04-16 12:05:16 +0200

    changed file location

--- moraba/Program.cs
+++ moraba/Program.cs
@@ -18,12 +18,7 @@ namespace moraba
             makePlayer();
             PlayGame();
             Board.printBoard();
-            string[] cowsToPlace = new string[] { "a0", "a3", "a6", "b1", "b3", "b5", "c2", "c3", "c4", "d0", "d1", "d2", "d4", "d5", "d6", "e2", "e3", "e4", "f1", "f3", "f5", "g0", "g3", "g6" };
-            for (int i = 0; i < 12; i++)
-            {
-                Console.WriteLine(Board.Placing(cowsToPlace[i], player2) == true);
-                //Assert.That(b.mainNodeList[i].Cow.Team == Team.DarkCow);
-            }
+           
 
 
             Console.Read();
