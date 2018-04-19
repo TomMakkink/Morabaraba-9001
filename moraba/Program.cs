b14f6f84fb7395ee3719b01aa184d16ca6b91eff
desluk <lukedestombe.ldt@gmail.com>
2018-04-19 16:15:38 +0200

    Changed the program.cs to make players,boards and the umpire

--- moraba/Program.cs
+++ moraba/Program.cs
@@ -7,32 +7,15 @@ namespace moraba
 {
     class Program
     {
-        public static Board Board = new Board();
-        public static Umpire Umpire = new Umpire(Board);
-        public static Player player1 = new Player ("test",Team.DarkCow);
-        public static Player player2 = new Player("test2", Team.LightCow);
+
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            makePlayer();
-            PlayGame();
-            Board.printBoard();
-
-            
-           
-
-
-
-            Console.Read();
-        }
-
-        static void PlayGame()
-        {
-            Umpire.play(player1, player2);
-        }
-        static void makePlayer ()
-        {
+            Board Board = new Board();
+            Umpire Umpire = new Umpire(Board);
+            Player player1;
+            Player player2;
             Console.WriteLine("Good day to the new generals that are about to partak in this glorious battle. May we have your names.");
             Console.WriteLine();
             Console.WriteLine("General one your name please:   ");
@@ -40,11 +23,23 @@ namespace moraba
             Console.WriteLine();
             Console.WriteLine("General two your name please:   ");
             string name2 = Console.ReadLine();
-            player1 = new Player(name1, Team.DarkCow);
-            player2 = new Player(name2, Team.LightCow);
+            player1 = new Player(name1, Team.DarkCow, Board);
+            player2 = new Player(name2, Team.LightCow, Board);
             Console.WriteLine("We will now start the battle prepare thyne selves.");
+           
+            
+            Board.printBoard();
 
+            Umpire.play(player1, player2);
+
+
+
+
+            Console.Read();
         }
+
+        
+        
 
       
 
